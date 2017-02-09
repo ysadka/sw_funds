fe807ec43e672c685fdc72c871ecb2ff4403d8bf
ysadka <ysadka@gmail.com>
2017-02-08 22:16:11 -0800

    specify sendgrid for mailers

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,14 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# Use Sendgrid for email
+ActionMailer::Base.smtp_settings = {
+  :user_name => 'yaron',
+  :password => ENV['SENDGRID_PASSWORD'],
+  :domain => 'travelfunds.us',
+  :address => 'smtp.sendgrid.net',
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
